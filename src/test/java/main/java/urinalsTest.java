ea3332972d942889cc7aef808df68c204525b9b8
adamshoaibk <akareems@asu.edu>
2022-10-27 20:45:55 -0700

    added test for hasConsequentOnes Function

--- src/test/java/main/java/urinalsTest.java
+++ src/test/java/main/java/urinalsTest.java
@@ -12,12 +12,11 @@ class urinalsTest {
     void isValidInputTrueAssertions() {
         System.out.println("====== Adam Shoaib K == TEST ONE EXECUTED =======");
         urinals ur = new urinals();
-        Assertions.assertEquals( true , ur.isValidString("10110"));
+        Assertions.assertEquals( true , ur.isValidString("1010"));
         Assertions.assertEquals( true , ur.isValidString("0"));
         Assertions.assertEquals( true , ur.isValidString("1"));
         Assertions.assertEquals( true , ur.isValidString("00"));
-        Assertions.assertEquals( true , ur.isValidString("11"));
-        Assertions.assertEquals( true , ur.isValidString("111111111"));
+        Assertions.assertEquals( true , ur.isValidString("0101"));
         Assertions.assertEquals( true , ur.isValidString("000000000"));
     }
 
@@ -47,6 +46,27 @@ class urinalsTest {
         Assertions.assertEquals( false , ur.isValidString("-1"));
         Assertions.assertEquals( false , ur.isValidString("-0"));
         Assertions.assertEquals( false , ur.isValidString("-01001"));
-        Assertions.assertEquals( false , ur.isValidString("-11111"));
+    }
+
+    @Test
+    void isValidInputTwoConsequentOnes() {
+        System.out.println("====== Adam Shoaib K == TEST FOUR EXECUTED =======");
+        urinals ur = new urinals();
+        Assertions.assertEquals( false , ur.isValidString("011"));
+        Assertions.assertEquals( false , ur.isValidString("111"));
+        Assertions.assertEquals( false , ur.isValidString("010110"));
+    }
+
+    @Test
+    void hasConsequentOnes() {
+        System.out.println("====== Adam Shoaib K == TEST FIVE EXECUTED =======");
+        urinals ur = new urinals();
+        Assertions.assertEquals( true , ur.hasConsequentOnes("011"));
+        Assertions.assertEquals( true , ur.hasConsequentOnes("111"));
+        Assertions.assertEquals( false , ur.hasConsequentOnes("010"));
+        Assertions.assertEquals( false , ur.hasConsequentOnes("000"));
+        Assertions.assertEquals( false , ur.hasConsequentOnes("1"));
+        Assertions.assertEquals( false , ur.hasConsequentOnes("0"));
+
     }
 }
