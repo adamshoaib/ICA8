bfcbb60e469a7d2b8435374dad6db66f6dd6a137
adamshoaibk <akareems@asu.edu>
2022-10-27 20:14:07 -0700

    added more test cases for fn - isValidString

--- src/test/java/main/java/urinalsTest.java
+++ src/test/java/main/java/urinalsTest.java
@@ -9,16 +9,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class urinalsTest {
 
     @Test
-    void isValidInput() {
+    void isValidInputTrueAssertions() {
         System.out.println("====== Adam Shoaib K == TEST ONE EXECUTED =======");
         urinals ur = new urinals();
         Assertions.assertEquals( true , ur.isValidString("10110"));
+        Assertions.assertEquals( true , ur.isValidString("0"));
+        Assertions.assertEquals( true , ur.isValidString("1"));
+        Assertions.assertEquals( true , ur.isValidString("00"));
+        Assertions.assertEquals( true , ur.isValidString("11"));
+        Assertions.assertEquals( true , ur.isValidString("111111111"));
+        Assertions.assertEquals( true , ur.isValidString("000000000"));
     }
 
     @Test
-    void isValidInputDifferentNumbers() {
-        System.out.println("====== Adam Shoaib K == TEST 2 EXECUTED =======");
+    void isValidInputFalseAssertions() {
+        System.out.println("====== Adam Shoaib K == TEST TWO EXECUTED =======");
         urinals ur = new urinals();
         Assertions.assertEquals( false , ur.isValidString("101120"));
+        Assertions.assertEquals( false , ur.isValidString("1011A0"));
+        Assertions.assertEquals( false , ur.isValidString("/"));
+        Assertions.assertEquals( false , ur.isValidString("A"));
+        Assertions.assertEquals( false , ur.isValidString("a"));
+        Assertions.assertEquals( false , ur.isValidString("9"));
+        Assertions.assertEquals( false , ur.isValidString(""));
+        Assertions.assertEquals( false , ur.isValidString(" "));
+        Assertions.assertEquals( false , ur.isValidString("-1"));
+        Assertions.assertEquals( false , ur.isValidString("-0"));
+        Assertions.assertEquals( false , ur.isValidString("o")); // should not consider letter "o"
+        Assertions.assertEquals( false , ur.isValidString("O")); // should not consider letter "O"
+        Assertions.assertEquals( false , ur.isValidString("I"));
+    }
+
+    @Test
+    void isValidInputNegativeNumber() {
+        System.out.println("====== Adam Shoaib K == TEST THREE EXECUTED =======");
+        urinals ur = new urinals();
+        Assertions.assertEquals( false , ur.isValidString("-1"));
+        Assertions.assertEquals( false , ur.isValidString("-0"));
+        Assertions.assertEquals( false , ur.isValidString("-01001"));
+        Assertions.assertEquals( false , ur.isValidString("-11111"));
     }
 }
